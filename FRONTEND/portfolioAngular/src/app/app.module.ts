ba8879d0ec88094e1aa3b87f5a7720437297401b
Vero <veronievasguardia@gmail.com>
2023-03-25 16:59:42 -0300

    creando componentes de experiencia y educación

--- FRONTEND/portfolioAngular/src/app/app.module.ts
+++ FRONTEND/portfolioAngular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercadeComponent
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
